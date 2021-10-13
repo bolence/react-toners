e8b58f3b88eaa64cb4959a51b3f900de014d812b
bolence <bosko.boskovic@gmail.com>
2021-10-13 20:17:21 +0200

    added more routes

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::get('/limits', [App\Http\Controllers\HomeController::class, 'limits'])->name('limits')->middleware('auth');
 Route::get('/printers', [App\Http\Controllers\PrinterController::class, 'index'])->name('printers')->middleware('auth');
-Route::resource('orders', App\Http\Controllers\OrderController::class)->middleware('auth');
-Route::get('reports', [App\Http\Controllers\PdfController::class, 'index'])->middleware('auth');
+Route::resource('/orders', App\Http\Controllers\OrderController::class)->middleware('auth');
+Route::get('/reports', [App\Http\Controllers\PdfController::class, 'index'])->middleware('auth');
